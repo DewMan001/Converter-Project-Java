6cf79bed7bdc67b1282b445a24a858eff6ea16a4
James Burton <deweymanster@gmail.com>
2016-03-26 11:07:28 +0800

    Wrong main file, apparently
    Fixed now. Frame runs properly.
    
    Commit Album - Prom King by Skylar Spence
    YouTube: https://www.youtube.com/watch?v=QwvRy88s1Wk
    
    Buy direct:
    http://store.carparkrecords.com/products/551753-cak107-skylar-spence-prom-king

--- Converter(NewAndImproved2Pernt0h)/src/converter/newandimproved2pernt0h/converter.java
+++ Converter(NewAndImproved2Pernt0h)/src/converter/newandimproved2pernt0h/converter.java
@@ -31,27 +31,61 @@ public class converter extends javax.swing.JFrame {
         jTabbedPane2 = new javax.swing.JTabbedPane();
         jTabbedPane3 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
-        jRadioButton1 = new javax.swing.JRadioButton();
+        radbtn_FeetFrom = new javax.swing.JRadioButton();
+        radbtn_InchesFrom = new javax.swing.JRadioButton();
+        radbtn_MetersFrom = new javax.swing.JRadioButton();
+        radbtn_CentimetresFrom = new javax.swing.JRadioButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jRadioButton1.setText("jRadioButton1");
+        radbtn_FeetFrom.setText("Feet");
+
+        radbtn_InchesFrom.setText("Inches");
+
+        radbtn_MetersFrom.setText("Meters");
+
+        radbtn_CentimetresFrom.setText("Centimetres");
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 3, 16)); // NOI18N
+        jLabel1.setText("Convert from...");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addComponent(jRadioButton1)
-                .addContainerGap(202, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(radbtn_FeetFrom)
+                                .addGap(18, 18, 18)
+                                .addComponent(radbtn_MetersFrom))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(radbtn_InchesFrom)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(radbtn_CentimetresFrom))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addComponent(jLabel1)))
+                .addContainerGap(390, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(54, 54, 54)
-                .addComponent(jRadioButton1)
-                .addContainerGap(183, Short.MAX_VALUE))
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(radbtn_FeetFrom)
+                    .addComponent(radbtn_MetersFrom))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(radbtn_InchesFrom)
+                    .addComponent(radbtn_CentimetresFrom))
+                .addContainerGap(169, Short.MAX_VALUE))
         );
 
         jTabbedPane3.addTab("tab1", jPanel1);
@@ -106,10 +140,14 @@ public class converter extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTabbedPane jTabbedPane2;
     private javax.swing.JTabbedPane jTabbedPane3;
+    private javax.swing.JRadioButton radbtn_CentimetresFrom;
+    private javax.swing.JRadioButton radbtn_FeetFrom;
+    private javax.swing.JRadioButton radbtn_InchesFrom;
+    private javax.swing.JRadioButton radbtn_MetersFrom;
     // End of variables declaration//GEN-END:variables
 }
