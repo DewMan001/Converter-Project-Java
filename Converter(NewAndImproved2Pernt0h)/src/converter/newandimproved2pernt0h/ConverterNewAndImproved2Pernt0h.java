b429897329d04462b481eb1cffee985016004087
James Burton <deweymanster@gmail.com>
2016-03-26 08:52:17 +0800

    New project file in the old folder
    AND I've worked out how to put a proper form inside a tabbed thing!
    YEAH!

---
+++ Converter(NewAndImproved2Pernt0h)/src/converter/newandimproved2pernt0h/ConverterNewAndImproved2Pernt0h.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter.newandimproved2pernt0h;
+
+/**
+ *
+ * @author dewey
+ */
+public class ConverterNewAndImproved2Pernt0h {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
